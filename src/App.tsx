369ae0604a32c38c412ad574c5458f2ddc38bcca
Tulza <40633228+tulza@users.noreply.github.com>
2024-07-12 00:30:05 +1200

    Init setup
    - React TS + Swc (client side only)
    - Tailwind CSS
    - some file management
    - no default template
    - UOACS template!!!!

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center w-dvw h-dvh">
+      <div>
+        <img
+          src="/images/UOACS.png"
+          className="w-20 aspect-square test animate-spin"
+          alt="UOACS"
+        />
+      </div>
+      <h1 className="text-xl font-bold underline">omg UOACS</h1>
+      welcome
+    </div>
+  );
+}
+
+export default App;
