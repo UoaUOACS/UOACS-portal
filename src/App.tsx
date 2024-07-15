792ea9846994c4157940dd165bc127c909c2b877
Tulza <40633228+tulza@users.noreply.github.com>
2024-07-16 00:07:34 +1200

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,49 @@
-import { motion } from "framer-motion";
-
+import { domAnimation, LazyMotion, m } from 'framer-motion';
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center w-dvw h-dvh">
-      <motion.div
-        className="w-40 minecraft"
-        initial={{ rotateX: 0, rotateY: 0, rotateZ: 0 }}
-        animate={{ rotateX: 360, rotateY: 720, rotateZ: 0 }}
-        transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
-      >
-        <div
-          className="w-40 aspect-square absolute grid place-items-center border border-white"
-          style={{ transform: "translate3d(0,0,-5rem)" }}
+    <LazyMotion features={domAnimation}>
+      <div className="flex flex-col justify-center items-center w-dvw h-dvh">
+        <m.div
+          className="w-40 minecraft"
+          initial={{ rotateX: 0, rotateY: 0, rotateZ: 0 }}
+          animate={{ rotateX: 360, rotateY: 720, rotateZ: 0 }}
+          transition={{ duration: 10, repeat: Infinity, ease: 'linear' }}
         >
-          <UOACS />
-        </div>
-        <div
-          className="w-40 aspect-square absolute grid place-items-center border border-white"
-          style={{ transform: "translate3d(0,0,5rem)" }}
-        >
-          <UOACS />
-        </div>
-        <div
-          className="w-40 aspect-square absolute grid place-items-center border border-white"
-          style={{ transform: "translate3d(0,5rem,0) rotateX(90deg)" }}
-        >
-          <UOACS />
-        </div>
-        <div
-          className="w-40 aspect-square absolute grid place-items-center border border-white"
-          style={{ transform: "translate3d(0,-5rem,0) rotateX(90deg)" }}
-        >
-          <UOACS />
-        </div>
-      </motion.div>
-      <p className="z-10 text-black font-black before:bg-black before:w-full before:contents-['']">
-        UOACS
-      </p>
-    </div>
+          <div
+            className="w-40 aspect-square absolute grid place-items-center border border-white"
+            style={{ transform: 'translate3d(0,0,-5rem)' }}
+          >
+            <UOACS />
+          </div>
+          <div
+            className="w-40 aspect-square absolute grid place-items-center border border-white"
+            style={{ transform: 'translate3d(0,0,5rem)' }}
+          >
+            <UOACS />
+          </div>
+          <div
+            className="w-40 aspect-square absolute grid place-items-center border border-white"
+            style={{ transform: 'translate3d(0,5rem,0) rotateX(90deg)' }}
+          >
+            <UOACS />
+          </div>
+          <div
+            className="w-40 aspect-square absolute grid place-items-center border border-white"
+            style={{ transform: 'translate3d(0,-5rem,0) rotateX(90deg)' }}
+          >
+            <UOACS />
+          </div>
+        </m.div>
+        <p className="z-10 text-black font-black">UOACS</p>
+      </div>
+    </LazyMotion>
   );
 }
 
 const UOACS = () => (
-  <motion.img
-    initial={{ filter: "blur(300px)", rotate: 80 }}
-    animate={{ filter: "blur(0px)", rotate: 0 }}
+  <m.img
+    initial={{ filter: 'blur(300px)', rotate: 80 }}
+    animate={{ filter: 'blur(0px)', rotate: 0 }}
     transition={{ duration: 2 }}
     src="/images/UOACS.png"
     className="w-40"
